88befdd9634c4fb47c39a7c3238253d11942ff53
PanaLennyFS <117951425+PanaLennyFS@users.noreply.github.com>
2022-11-12 15:15:32 -0600

    Add files via upload

---
+++ factorial-break.js
@@ -0,0 +1,9 @@
+let num = 0;
+
+while (num <= 20) {
+  num++
+  if (num === 11) {
+    break;
+  }
+  console.log(num)
+}
---
+++ factorial-while.js
@@ -0,0 +1,6 @@
+num = 0;
+
+while (num < 10) {
+  num++
+  console.log(num)
+}
